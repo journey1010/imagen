8fac79f5c6c819795f68ddb7bf151de4c2518f36
journii gorle <ginopaflo001608@gmail.com>
2024-12-03 09:49:06 -0500

    list

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\form;
 use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,4 +18,5 @@ use App\Http\Controllers\AuthController;
 Route::post('login', [AuthController::class, 'login']);
 Route::post('/form', [form::class, 'store']);
 Route::get('/oficinas', [form::class, 'oficinas'])->middleware('auth:api');
+Route::get('/list', [form::class, 'list'])->middleware('auth:api');
 Route::post('/store-files', [form::class, 'storeFiles']);
